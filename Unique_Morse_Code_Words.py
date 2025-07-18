27ee739acd855b7a5758f6753592b428ce305385
ALI ABBAS MASHRIQI <aliabbasmashriqi@gmail.com>
2025-07-18 17:48:43 +0530

    This is working perfectly and it optimal and did it by my own no help needed

---
+++ Unique_Morse_Code_Words.py
@@ -0,0 +1,25 @@
+from typing import List
+morse_arr =[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+words = ["gin","zen","gig","msg"]
+class Solution:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        temp_set = set()
+        # arr_num = 0
+        # for num_char in range(97,123):
+        #     temp_dict[chr(num_char)] = morse_arr[arr_num]
+        #     arr_num+=1
+        # print(temp_dict)
+        # temp = ord('g')
+        # print(morse_arr[temp-ord('a')])
+        for word in words:
+            temp_list = []
+            for alpha in word:
+                temp_list.append(morse_arr[ord(alpha) - ord('a')])
+            temp = ''.join(temp_list)
+            temp_set.add(temp)
+            
+        print(temp_set)
+        return len(temp_set)
+      
+sol = Solution()
+sol.uniqueMorseRepresentations(words)  
