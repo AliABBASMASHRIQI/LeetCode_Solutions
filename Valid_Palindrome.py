01980a09e7d4e59db329cc8610aa0bf729d250ac
ALI ABBAS MASHRIQI <aliabbasmashriqi@gmail.com>
2025-09-17 10:19:00 +0530

    optimal and easily done in like 10 minutes a very good question just not optimal in space where join should not be used and isalnum should be used in every condition for it to work

---
+++ Valid_Palindrome.py
@@ -0,0 +1,23 @@
+s = "aa"
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = ''.join(c.lower() for c in s if c.isalnum())
+        i = 0
+        j = len(s)-1
+        if len(s) == 0:
+            print(True)
+            return True
+        while i != j and i < j:
+            print(s[i],s[j])
+            if s[i]!=s[j]:
+                print(False)
+                return False
+            i+=1
+            j-=1
+        print(True)
+        return True
+        
+ #optimal and easily done in like 10 minutes a very good question        
+
+sol = Solution()
+sol.isPalindrome(s)
