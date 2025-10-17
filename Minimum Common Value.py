847c8672c0e46e9e3d6c920f8d5d85aabb647519
ALI ABBAS MASHRIQI <aliabbasmashriqi@gmail.com>
2025-10-17 09:07:00 +0530

    optimal and it is working fine and done by me

---
+++ Minimum Common Value.py
@@ -0,0 +1,11 @@
+from typing import List
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        temp_set1 = set(nums1)
+        temp_set2 = set(nums2)
+        common = temp_set1 & temp_set2
+        if not common:
+            return -1  
+        return min(common)
+
+# optimal and it is working fine and done by me
